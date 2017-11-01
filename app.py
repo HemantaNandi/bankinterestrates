4e9dd8719cf58b9cfd944e2868c2be4ee90b48ac
Hemanta Nandi <hemantanandi2@gmail.com>
2017-11-01 19:23:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    #if req.get("result").get("action") != "shipping.cost":
-        #return {}
+    if req.get("result").get("action") != "shipping.cost":
+        return {}
     result = req.get("result")
     parameters = result.get("parameters")
     zone = parameters.get("bank-name")
